58b6fd9a7a5ed8ece61c9f3572d7fa773336f816
Keti88 <105712518+Keti88@users.noreply.github.com>
2022-05-22 11:23:42 +0200

    Add files via upload

---
+++ HelloWorld/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+namespace Helloworld
+{
+    class Program
+    {
+       /* public static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Hello World!");
+            //Console.Write("Press any key to continue . . . ");
+            
+
+            
+        }
+        System.Threading.Timer timer = new System.Threading.Timer(timerA, null,5000, 5000 );
+        private void timerA(object state)
+        {
+            Environment.Exit(0);
+        }*/
+        static void Main(string[] args)
+        {
+            Play();
+        }
+
+        static void Play()
+        {
+            Console.WriteLine("Announce Program");
+            Console.WriteLine("Close Program Announcement");
+            Thread.Sleep(1000);
+            Environment.Exit(2000);
+        }
+    }
+}
